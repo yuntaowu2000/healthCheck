c58dc324c2adf66d2c2959e78953b14f539ddf97
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2021-06-25 14:43:14 -0700

    added email sending

--- index.js
+++ index.js
@@ -5,22 +5,45 @@ const fs = require('fs');
 const serverData = fs.readFileSync('./.jsonfiles/server.json', 'utf8');
 const jsonObj = JSON.parse(serverData);
 
-exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello!' }),
-  };
-
-  callback(null, response); 
-};
+const nodemailer = require('nodemailer');
+const emailID = fs.readFileSync('./.jsonfiles/email.json', 'utf8');
+const emailObj = JSON.parse(emailID);
+const transporter = nodemailer.createTransport({
+  host: emailObj.server,
+  port: emailObj.port,
+  auth: {
+    user: emailObj.user,
+    pass: emailObj.key
+  }
+})
 
 exports.check = async (event, context, callback) => {
   let result = [];
+  let failed = [];
   for (let sd of jsonObj) {
     const res = await agent.head(sd.host).connect(sd.ip);
-    console.log(res);
     result.push({[sd.host] : res.statusCode});
+    if (res.statusCode != 200) {
+      let f = {"name" : sd.name, "host" : sd.host, "ip" : sd.ip, "status code" : res.statusCode};
+      failed.push(f);
+    }
   }
   
-  callback(null, JSON.stringify(result));
+  if (failed.length > 0) {
+    await transporter.sendMail({
+      from: emailObj.user,
+      to: emailObj.to,
+      subject: "Server connection failed",
+      text: JSON.stringify(failed)
+    });
+  } else {
+    await transporter.sendMail({
+      from: emailObj.user,
+      to: emailObj.to,
+      subject: "Server connection test finished",
+      text: "success"
+    });
+  }
+
+  callback(null, JSON.stringify(failed));
 };
